5d9c7e7627bfa08ef5df6521964f25a8bd250c13
Zhirui <jerrio20@outlook.com>
2021-04-24 17:03:25 +0800

    feat: add load balance

---
+++ loadbalancer-user/src/test/java/com/icewould/loadbalancer/user/LoadbalancerUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icewould.loadbalancer.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadbalancerUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
